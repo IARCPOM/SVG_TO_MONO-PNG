af263c093a3ea11bed069be55ad57282f7665612
Al-Amin Quyyum <iarcpom@gmail.com>
2018-09-15 10:59:19 +0600

    TEST

---
+++ SVG_TO_monoPNG2.bat
@@ -0,0 +1,33 @@
+@echo off
+MODE 80,10
+TITLE SVG TO MONOCHROME PNG
+setlocal ENABLEDELAYEDEXPANSION
+set path=%path%;C:\Program Files\ImageMagick-7.0.7-Q16
+echo Drag Directory
+set /p d=
+cd /D "%d%"
+
+mkdir "ALL_MY_MONO_PNG"
+cls
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /a-d *.svg`) DO (
+	
+	FOR %%x in ("%%F") do (
+		TITLE %%~nx.svg
+		ECHO %%~nx.svg : %%~nx.png
+		magick convert "%%~nx.svg" -monochrome "ALL_MY_MONO_PNG\%%~nx.png"
+	)
+	
+)
+TITLE DONE
+ECHO -----------------------------------------
+ECHO   ########   #######  ##    ## ######## 
+ECHO   ##     ## ##     ## ###   ## ##       
+ECHO   ##     ## ##     ## ####  ## ##       
+ECHO   ##     ## ##     ## ## ## ## ######   
+ECHO   ##     ## ##     ## ##  #### ##       
+ECHO   ##     ## ##     ## ##   ### ##       
+ECHO   ########   #######  ##    ## ######## 
+ECHO -----------------------------------------
+pause>NUL
+
+exit
